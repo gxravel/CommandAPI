e1eed0a301efdae2b38620d987932914272ce921
Ravil Gabdullin <gvg_ravil@mail.ru>
2020-10-19 15:28:31 +0300

    Completed api endpoints

--- src/CommandAPI/Data/SqlCommandAPIRepo.cs
+++ src/CommandAPI/Data/SqlCommandAPIRepo.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using CommandAPI.Models;
 using System.Linq;
@@ -12,14 +13,23 @@ namespace CommandAPI.Data
         {
             _context = context;
         }
+
         public void CreateCommand(Command command)
         {
-            throw new System.NotImplementedException();
+            if (command == null)
+            {
+                throw new ArgumentNullException(nameof(command));
+            }
+            _context.CommandItems.Add(command);
         }
 
         public void DeleteCommand(Command command)
         {
-            throw new System.NotImplementedException();
+            if (command == null)
+            {
+                throw new ArgumentNullException(nameof(command));
+            }
+            _context.CommandItems.Remove(command);
         }
 
         public IEnumerable<Command> GetAllCommands()
@@ -34,12 +44,12 @@ namespace CommandAPI.Data
 
         public bool SaveChanges()
         {
-            throw new System.NotImplementedException();
+            return (_context.SaveChanges() >= 0);
         }
 
         public void UpdateCommand(Command command)
         {
-            throw new System.NotImplementedException();
+            // We don't need to do anything here
         }
     }
 }
--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json.Serialization;
+using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -32,7 +34,11 @@ namespace CommandAPI
 
             services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(builder.ConnectionString));
 
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(s => 
+            {
+                s.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            });
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
         }
 
