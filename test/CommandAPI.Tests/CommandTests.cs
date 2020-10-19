48fb97e67eaa1647501ef121fa98f21392bcc40b
Ravil Gabdullin <gvg_ravil@mail.ru>
2020-10-19 18:17:36 +0300

    Made the first unit tests

---
+++ test/CommandAPI.Tests/CommandTests.cs
@@ -0,0 +1,47 @@
+using System;
+using Xunit;
+using CommandAPI.Models;
+
+namespace CommandAPI.Tests
+{
+    public class CommandTests : IDisposable
+    {
+        Command testCommand;
+
+        public CommandTests()
+        {
+            testCommand = new Command
+            {
+                HowTo = "Do something",
+                Platform = "Some Platform",
+                CommandLine = "Some commandLine"
+            };
+        }
+
+        public void Dispose()
+        {
+            testCommand = null;
+        }
+
+        [Fact]
+        public void CanChangeHowTo()
+        {
+            testCommand.HowTo = "Execute Unit Tests";
+            Assert.Equal("Execute Unit Tests", testCommand.HowTo);
+        }
+
+        [Fact]
+        public void CanChangePlatform()
+        {
+            testCommand.Platform = "Execute Unit Tests";
+            Assert.Equal("Execute Unit Tests", testCommand.Platform);
+        }
+
+         [Fact]
+        public void CanChangeCommandLine()
+        {
+            testCommand.CommandLine = "Execute Unit Tests";
+            Assert.Equal("Execute Unit Tests", testCommand.CommandLine);
+        }
+    }
+}
